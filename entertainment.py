87234d01537fc3f9629e1d813042ddd5e8c0e7ba
Lightninggabe1 <lightninggabe191@gmail.com>
2024-05-23 14:15:35 +0800

    Create entertainment.py

---
+++ entertainment.py
@@ -0,0 +1,9 @@
+def get_daily_movies():
+    print('Retrieving the movie set to play on today\'s flight...')
+    return ['Whoo Hoo', 'Nomadland', 'Roma', 'Black Widow', 'Spiral']
+
+
+def get_licensed_movies():
+    print('Retrieving the list of licenses movies from the database...')
+    licensed_movies = ['Whoo Hoo', 'Nomadland', 'Roma']
+    return licensed_movies
