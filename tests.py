ceeba3b0da0fa582eac4ab5e466ea2b1e826e3e8
Lightninggabe1 <lightninggabe191@gmail.com>
2024-05-23 14:44:57 +0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import entertainment
+import feedback
 
 class EntertainmentSystemTests(unittest.TestCase):
 
@@ -42,3 +43,16 @@ class EntertainmentSystemTests(unittest.TestCase):
 
 unittest.main()
 
+
+
+class CustomerFeedbackTests(unittest.TestCase):
+
+  # Write your code below:
+  @unittest.expectedFailure
+  def test_survey_form(self):
+    self.assertEqual(feedback.issue_survey(), 'Success')
+
+  def test_complaint_form(self):
+    self.assertEqual(feedback.log_customer_complaint(), 'Success')
+
+unittest.main()
